e95cf0d544bf8af1493167eb8cdbee05aca14215
Mohit Goplani <goplanimohit510@gmail.com>
2023-11-19 14:00:27 +0530

    Adding routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::view("/about", 'about')->name('about');
+Route::view("/history",'history')->name('history');
+Route::view("/awards",'awards')->name('awards');
+Route::view("/contact",'contact')->name('contact');
