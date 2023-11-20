59528686cfee77b22cd342de86bd017d50ac64bd
Mohit Goplani <goplanimohit510@gmail.com>
2023-11-20 20:31:46 +0530

    Adding history page

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,5 @@
 <footer>
-    <div class="text-white">Copyright 2023</div>
+    <div class="text-white">
+        <h4><center>Copyright 2023 | <a href="https://www.instagram.com/virat.kohli/feed/?hl=en">instagram</a></a>
+    </div>
 </footer>
